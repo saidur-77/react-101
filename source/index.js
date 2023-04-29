baeceb0d0f1d7ae8337e016d6e6b5f695208db07
Sai Durve <saidurve@gmail.com>
2023-04-29 21:02:55 +1000

    RCT-2 Clean up + including multiple elements is jsx

--- source/index.js
+++ source/index.js
@@ -1,1 +1,16 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"));
+// JSX = Javascript XML 
+// Renders into JSON which is then passed to webpage
+
+// 'ClassName' instead of just 'class'
+// Multiple elements can be put in if they are put inside a single parrent element
+
+const page = (
+    <div>
+        <h1 className="header">Hello, everyone!</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+);
